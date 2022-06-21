3b12e075b45640954dbd7eef5bbce1e11647b587
maireadwhite <mairead.white@codeinstitute.net>
2022-06-21 23:00:35 +0000

    Function for startGame has been created and modified

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
 function startGame() {
-
+    console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
-
 function setNextQuestion() {
 
 }
 
 function selectAnswer() {
-    
+
 }
