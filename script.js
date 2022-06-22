4594ea72b0eaa1d96e663f1400362f078f37b7a5
maireadwhite <mairead.white@codeinstitute.net>
2022-06-22 23:07:20 +0000

    Tried to find error to fix answers that have disappearred but have now lost all text

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
-let shuffledQuestions, currentQuestionindex
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
-    currentQuestionindex++
+    currentQuestionIndex++
     setNextQuestion()
 })
 
 function startGame() {
     startButton.classList.add('hide')
     shuffledQuestions = questions.sort(() => random())
-    currentQuestionindex = 0
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 function setNextQuestion() {
     resetState()
-    showQuestion(shuffledQuestions[currentQuestionindex])
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
@@ -53,7 +53,7 @@ function selectAnswer(e) {
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
-    if (shuffledQuestions.length > currentQuestionindex + 1) {
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide')
     } else {
         startButton.innerText = 'Restart'
@@ -66,7 +66,7 @@ function setStatusClass(element, correct) {
     if (correct) {
         element.classList.add('correct')
 
-    } else  {element.classList.add('incorrect')
+    } else {element.classList.add('incorrect')
     }
 }
 
@@ -77,6 +77,7 @@ function clearStatusClass(element) {
 
 
 const questions = [
+   
     {
         question: 'What depth is the Atlantic ocean at its deepest part?',
         answer: [
@@ -85,4 +86,31 @@ const questions = [
 
         ]
     },
+
+    {
+        question: 'What depth is the English channel at its deepest part?',
+        answer: [
+            { Text: '1000ft', correct: true },
+            { Text: '3000ft', correct: false }
+
+        ]
+    },
+
+    {
+        question: 'What depth is the Pacific ocean at its deepest part?',
+        answer: [
+            { Text: '1000ft', correct: true },
+            { Text: '3000ft', correct: false }
+
+        ]
+    },
+
+    {
+        question: 'What depth is the Indian ocean at its deepest part?',
+        answer: [
+            { Text: '1000ft', correct: true },
+            { Text: '3000ft', correct: false }
+
+        ]
+    }
 ]
